3e30f093c7f05835cad65874b5687bd582cd4dcc
c0a240883a <c0a240883a@edu.teu.ac.jp>
2025-06-24 17:21:28 +0900

    演習3:途中

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -56,6 +56,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire=(5,0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -78,6 +79,8 @@ class Bird:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         self.rct.move_ip(sum_mv)
+        if sum_mv!=[0,0]:
+            self.dire=sum_mv
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
@@ -157,6 +160,24 @@ class Score:
         pg.display.update()  # 更新
 
 
+class Explosion:
+    """
+    爆発に関するクラス
+    """
+    def __init__(self, obj: pg.Rect):
+        self.imgs = [pg.image.load("fig/explosion.gif"),pg.transform.flip(pg.image.load("fig/explosion.gif"), True, True)]
+        self.rct = self.imgs[0].get_rect()
+        self.rct.center = obj.center
+        self.life = 20
+def update(self, screen: pg.Surface):
+        screen.blit(self.imgs[self.life//10%2], self.rct)
+        self.life -= 1
+        return self.life > 0
+
+# def check_bound(rct: pg.Rect) -> bool:
+#     return 0 <= rct.left and rct.right <= WIDTH and 0 <= rct.top and rct.bottom <= HEIGHT
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -169,6 +190,7 @@ def main():
     score = Score()  # Score呼び出し
     beam = None  # ゲーム初期化時にはビームは存在しない
     beams=[]
+    explosions = []
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -200,10 +222,12 @@ def main():
                         bombs[i]=None
                         beams[j]=None
                         bird.change_img(6, screen)
+                        explosions.append(Explosion(bomb.rct))
                         score.score+=1
         bombs = [bomb for bomb in bombs if bomb is not None]
         beams = [beam for beam in beams if beam is not None]
-        
+        explosions = [Ex for Ex in explosions if  Ex.life > 0]
+    
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
@@ -215,6 +239,9 @@ def main():
             bomb.update(screen)
         pg.display.update()
         score.update(screen)
+        for explosion in explosions:
+            if explosion > 0:
+                explosion.update(screen)
         tmr += 1
         clock.tick(50)
 
